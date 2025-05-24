0f4bfbea342fe4669da7182961dab39e20bc8155
Mohit Ambe <mohitambe77@gmail.com>
2025-05-24 11:22:07 -0400

    Empty Calendar

---
+++ 04/solution.py
@@ -0,0 +1,20 @@
+file = open("input.txt", "r")
+data = [line.strip() for line in file.readlines()]
+file.close()
+
+
+def part1():
+    print()
+
+
+def part2():
+    print()
+
+
+def part3():
+    print()
+
+
+part1()
+part2()
+part3()
