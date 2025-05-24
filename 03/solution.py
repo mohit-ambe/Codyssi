eab86976b5d948496b44c0394ba199ae5459212d
Mohit Ambe <mohitambe77@gmail.com>
2025-05-24 12:01:41 -0400

    Problem 3

--- 03/solution.py
+++ 03/solution.py
@@ -2,17 +2,29 @@ file = open("input.txt", "r")
 data = [line.strip() for line in file.readlines()]
 file.close()
 
+ranges = [list(map(int, x.replace(" ", "-").split("-"))) for x in data]
+
 
 def part1():
-    print()
+    print(sum(r - l + 1 for l, r in [list(map(int, x.split("-"))) for x in " ".join(data).split()]))
 
 
 def part2():
-    print()
+    boxes = 0
+    for l1, r1, l2, r2 in ranges:
+        boxes += len(set(range(l1, r1 + 1)) | set(range(l2, r2 + 1)))
+    print(boxes)
 
 
 def part3():
-    print()
+    boxes = 0
+    for i in range(len(ranges) - 1):
+        labelled = set()
+        for j in range(i, i + 2):
+            l1, r1, l2, r2 = ranges[j]
+            labelled |= set(range(l1, r1 + 1)) | set(range(l2, r2 + 1))
+        boxes = max(boxes, len(labelled))
+    print(boxes)
 
 
 part1()
