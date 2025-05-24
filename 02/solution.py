9ccc2330b0491957a2a737ec3a73972c68efa907
Mohit Ambe <mohitambe77@gmail.com>
2025-05-24 11:46:51 -0400

    Problem 2

--- 02/solution.py
+++ 02/solution.py
@@ -1,18 +1,34 @@
+import re
+
 file = open("input.txt", "r")
 data = [line.strip() for line in file.readlines()]
 file.close()
 
+funcs = list(map(int, re.findall("\d+", ' '.join(data[:3]))))
+nums = sorted(list(map(int, data[4:])))
+
+
+def apply_functions(x):
+    return x ** funcs[2] * funcs[1] + funcs[0]
+
 
 def part1():
-    print()
+    print(apply_functions(nums[len(nums) // 2]))
 
 
 def part2():
-    print()
+    print(apply_functions(sum(x for x in nums if x % 2 == 0)))
 
 
 def part3():
-    print()
+    prev = 0
+
+    for num in nums:
+        if apply_functions(num) > 15_000_000_000_000:
+            break
+        prev = num
+
+    print(prev)
 
 
 part1()
