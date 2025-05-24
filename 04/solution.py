74612d5a7c3a69f0d1ad73bb58cc796eed0eefd8
Mohit Ambe <mohitambe77@gmail.com>
2025-05-24 15:09:55 -0400

    Problem 4

--- 04/solution.py
+++ 04/solution.py
@@ -3,16 +3,38 @@ data = [line.strip() for line in file.readlines()]
 file.close()
 
 
+def memory(line):
+    return sum(ord(s) - ord('A') + 1 if s.isalpha() else int(s) for s in line)
+
+
 def part1():
-    print()
+    print(memory(''.join(data)))
 
 
 def part2():
-    print()
+    units = 0
+    for line in data:
+        chars = len(line) // 10
+        units += memory(line[:chars] + str(len(line) - 2 * chars) + line[-chars:])
+    print(units)
 
 
 def part3():
-    print()
+    units = 0
+    for line in data:
+        count = 0
+        char = ''
+        encoded = ""
+        for c in line:
+            if c != char:
+                encoded += char + str(count)
+                char = c
+                count = 1
+            else:
+                count += 1
+        encoded += char + str(count)
+        units += memory(encoded)
+    print(units)
 
 
 part1()
