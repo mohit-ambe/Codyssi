482c3af5ea684e292c2e891ab69aba58888ba3bf
Mohit Ambe <mohitambe77@gmail.com>
2025-05-25 15:33:15 -0400

    Problem 8

--- 08/solution.py
+++ 08/solution.py
@@ -4,15 +4,55 @@ file.close()
 
 
 def part1():
-    print()
+    print(len([x for x in "".join(data) if x.isalpha()]))
 
 
 def part2():
-    print()
+    lines = data.copy()
+    chars = 0
+    for line in lines:
+        change = True
+        while change:
+            change = False
+            i = 0
+            while i < len(line) - 1:
+                if line[i].isdigit():
+                    if line[i + 1].isalpha() or line[i + 1] == '-':
+                        change = True
+                        line = line[:i] + line[i + 2:]
+                        break
+                elif line[i].isalpha() or line[i] == '-':
+                    if line[i + 1].isdigit():
+                        change = True
+                        line = line[:i] + line[i + 2:]
+                        break
+                i += 1
+        chars += len(line)
+    print(chars)
 
 
 def part3():
-    print()
+    lines = data.copy()
+    chars = 0
+    for line in lines:
+        change = True
+        while change:
+            change = False
+            i = 0
+            while i < len(line) - 1:
+                if line[i].isdigit():
+                    if line[i + 1].isalpha():
+                        change = True
+                        line = line[:i] + line[i + 2:]
+                        break
+                elif line[i].isalpha():
+                    if line[i + 1].isdigit():
+                        change = True
+                        line = line[:i] + line[i + 2:]
+                        break
+                i += 1
+        chars += len(line)
+    print(chars)
 
 
 part1()
