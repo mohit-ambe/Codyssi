338cfb0d43485e179ec74b9b4f60ffb716db9195
Mohit Ambe <mohitambe77@gmail.com>
2025-05-24 22:50:13 -0400

    Problem 6

--- 06/solution.py
+++ 06/solution.py
@@ -4,15 +4,29 @@ file.close()
 
 
 def part1():
-    print()
+    print(len([x for x in data[0] if x.isalpha()]))
 
 
 def part2():
-    print()
+    lower = sum([ord(x) - ord('a') + 1 for x in data[0] if x.isalpha() and x.islower()])
+    upper = sum([ord(x) - ord('A') + 1 + 26 for x in data[0] if x.isalpha() and x.isupper()])
+    print(lower + upper)
 
 
 def part3():
-    print()
+    value = 0
+    total = 0
+    for char in data[0]:
+        if char.isalpha():
+            if char.islower():
+                v = ord(char) - ord('a') + 1
+            else:
+                v = ord(char) - ord('A') + 1 + 26
+        else:
+            v = (value * 2 - 5) % 52
+        value = v
+        total += v
+    print(total)
 
 
 part1()
